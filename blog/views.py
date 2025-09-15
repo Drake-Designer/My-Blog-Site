f3342547f546aba9f28cf07bfbc20bd200ec398e
Drake <dariocostarelli88@gmail.com>
2025-09-15 14:18:38 +0100

    Add sample posts data and basic views (index, all posts, post detail)

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,53 @@
 from django.shortcuts import render
+from datetime import date
+
+posts = [
+    {
+        "slug": "balancing-code-and-music",
+        "image": "me-coding.png",
+        "author": "Dario",
+        "date": date(2025, 9, 15),
+        "title": "Balancing Code and Music",
+        "excerpt": "How coding and music feed each other in my day-to-day.",
+        "content": """
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe similique velit nisi ad perspiciatis beatae quo repudiandae et inventore sed eligendi minima maxime reprehenderit nostrum, maiores architecto minus laborum in.
+        
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe similique velit nisi ad perspiciatis beatae quo repudiandae et inventore sed eligendi minima maxime reprehenderit nostrum, maiores architecto minus laborum in.
+
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe similique velit nisi ad perspiciatis beatae quo repudiandae et inventore sed eligendi minima maxime reprehenderit nostrum, maiores architecto minus laborum in.
+        """
+    },
+    {
+        "slug": "my-musical-journey",
+        "image": "me-drumming.png",
+        "author": "Dario",
+        "date": date(2024, 5, 10),
+        "title": "My Musical Journey",
+        "excerpt": "From the first drum hits to today: why rhythm shapes how I learn and build.",
+        "content": """
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe similique velit nisi ad perspiciatis beatae quo repudiandae et inventore sed eligendi minima maxime reprehenderit nostrum, maiores architecto minus laborum in.
+        
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe similique velit nisi ad perspiciatis beatae quo repudiandae et inventore sed eligendi minima maxime reprehenderit nostrum, maiores architecto minus laborum in.
+
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe similique velit nisi ad perspiciatis beatae quo repudiandae et inventore sed eligendi minima maxime reprehenderit nostrum, maiores architecto minus laborum in.
+        """
+    },
+    {
+        "slug": "my-gig-with-heat",
+        "image": "heat-gig.png",
+        "author": "Dario",
+        "date": date(2023, 11, 20),
+        "title": "My Gig with HEAT",
+        "excerpt": "Notes from the stage: what a live show taught me about focus and flow.",
+        "content": """
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe similique velit nisi ad perspiciatis beatae quo repudiandae et inventore sed eligendi minima maxime reprehenderit nostrum, maiores architecto minus laborum in.
+        
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe similique velit nisi ad perspiciatis beatae quo repudiandae et inventore sed eligendi minima maxime reprehenderit nostrum, maiores architecto minus laborum in.
+
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe similique velit nisi ad perspiciatis beatae quo repudiandae et inventore sed eligendi minima maxime reprehenderit nostrum, maiores architecto minus laborum in.
+        """
+    }
+]
 
 # Create your views here.
 
