e46bb9e374463db6ee0d6122b72a38af94855e88
Drake <dariocostarelli88@gmail.com>
2025-10-04 12:03:22 +0100

    Add Post admin, switch views to Post model, fix template field names

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,14 @@ from .models import Post, Author, Tag
 
 # Register your models here.
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'date')
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ('title', 'content')
+    list_filter = ('author', 'tags', 'date')
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)
--- blog/views.py
+++ blog/views.py
@@ -1,77 +1,27 @@
-from django.shortcuts import render
-from datetime import date
+from multiprocessing.util import LOGGER_NAME
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-all_posts = [
-    {
-        "slug": "balancing-code-and-music",
-        "image": "me-coding.png",
-        "author": "Dario",
-        "date": date(2025, 9, 15),
-        "title": "Balancing Code and Music",
-        "excerpt": "How coding and music feed each other in my day-to-day.",
-        "content": """
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe similique velit nisi ad perspiciatis beatae quo repudiandae et inventore sed eligendi minima maxime reprehenderit nostrum, maiores architecto minus laborum in.
-
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe similique velit nisi ad perspiciatis beatae quo repudiandae et inventore sed eligendi minima maxime reprehenderit nostrum, maiores architecto minus laborum in.
-
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe similique velit nisi ad perspiciatis beatae quo repudiandae et inventore sed eligendi minima maxime reprehenderit nostrum, maiores architecto minus laborum in.
-        """
-    },
-    {
-        "slug": "my-musical-journey",
-        "image": "me-drumming.png",
-        "author": "Dario",
-        "date": date(2024, 5, 10),
-        "title": "My Musical Journey",
-        "excerpt": "From the first drum hits to today: why rhythm shapes how I learn and build.",
-        "content": """
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe similique velit nisi ad perspiciatis beatae quo repudiandae et inventore sed eligendi minima maxime reprehenderit nostrum, maiores architecto minus laborum in.
-
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe similique velit nisi ad perspiciatis beatae quo repudiandae et inventore sed eligendi minima maxime reprehenderit nostrum, maiores architecto minus laborum in.
-
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe similique velit nisi ad perspiciatis beatae quo repudiandae et inventore sed eligendi minima maxime reprehenderit nostrum, maiores architecto minus laborum in.
-        """
-    },
-    {
-        "slug": "my-gig-with-heat",
-        "image": "heat-gig.png",
-        "author": "Dario",
-        "date": date(2023, 11, 20),
-        "title": "My Gig with HEAT",
-        "excerpt": "Notes from the stage: what a live show taught me about focus and flow.",
-        "content": """
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe similique velit nisi ad perspiciatis beatae quo repudiandae et inventore sed eligendi minima maxime reprehenderit nostrum, maiores architecto minus laborum in.
-
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe similique velit nisi ad perspiciatis beatae quo repudiandae et inventore sed eligendi minima maxime reprehenderit nostrum, maiores architecto minus laborum in.
-
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe similique velit nisi ad perspiciatis beatae quo repudiandae et inventore sed eligendi minima maxime reprehenderit nostrum, maiores architecto minus laborum in.
-        """
-    }
-]
-
-
-def get_date(post):
-    return post["date"]
 
 # Create your views here.
 
 
 def starting_page(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
+    latest_posts = Post.objects.all().order_by('-date')[:3]
     return render(request, "blog/index.html", {
         "posts": latest_posts
     })
 
 
 def posts(request):
+    all_posts = Post.objects.all().order_by('-date')
     return render(request, "blog/all-posts.html", {
         "all_posts": all_posts
     })
 
 
 def post_detail(request, slug):
-    identified_post = next(post for post in all_posts if post["slug"] == slug)
+    identified_post = get_object_or_404(Post, slug=slug)
     return render(request, "blog/post-detail.html", {
         "post": identified_post
     })
