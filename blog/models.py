fdf4347811a9ca5ac38fa759aa85d7a9fc8f0d9d
Drake <dariocostarelli88@gmail.com>
2025-10-14 08:48:03 +0100

    Migrate Post image field to Cloudinary for cloud storage

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinLengthValidator
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
@@ -23,7 +24,7 @@ class Author(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=150)
     excerpt = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='posts', null=True,)
+    image = CloudinaryField("image", default="placeholder")
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
